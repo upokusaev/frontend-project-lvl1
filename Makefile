d0a99c3adcefaafb2882c9d288962a2c4da86ae1
Yury Pokusaev <upokusaev@gmail.com>
2019-07-27 23:33:02 +0400

    Step 4

--- Makefile
+++ Makefile
@@ -6,3 +6,5 @@ start:
 	npx babel-node src/bin/brain-games.js
 publish:
 	npm publish --dry-run
+lint:
+	npx eslint src
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const askName = () => {
-    const userName = readlineSync.question("What's your name?\n");
-    console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question("What's your name?\n");
+  console.log(`Hello, ${userName}!`);
 };
