beff6ddce143cce562e0ba4f031b760dae09b696
Yury Pokusaev <upokusaev@gmail.com>
2019-08-05 14:58:58 +0400

    Fixed bugs progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,15 +7,15 @@ const progressionLength = 10;
 const generateGameData = () => {
   const firstNumber = getRandomNum(1, 9);
   const step = getRandomNum(2, 7);
-  const hiddenNumberIndex = getRandomNum(1, progressionLength);
+  const hiddenNumberIndex = getRandomNum(0, progressionLength - 1);
   const genProgression = (currentNumber, lengthCounter, strProgression) => {
-    if (lengthCounter > progressionLength) return strProgression;
+    if (lengthCounter > progressionLength - 1) return strProgression.trim();
     if (lengthCounter === hiddenNumberIndex) {
       return genProgression(currentNumber + step, lengthCounter + 1, `${strProgression} ..`);
     }
     return genProgression(currentNumber + step, lengthCounter + 1, `${strProgression} ${currentNumber + step}`);
   };
-  const question = genProgression(firstNumber, 1, '');
+  const question = genProgression(firstNumber - step, 0, '');
   const answer = (step * hiddenNumberIndex) + firstNumber;
   return buildData(question, answer);
 };
